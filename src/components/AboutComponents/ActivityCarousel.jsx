c160db4598c1ae6fe28a2681895667ee160cf297
wjtim <wjtimmermans15@gmail.com>
2025-01-24 14:54:12 -0800

    Added accessible navigation and improved alt text for image sliders

--- src/components/AboutComponents/ActivityCarousel.jsx
+++ src/components/AboutComponents/ActivityCarousel.jsx
@@ -1,92 +1,120 @@
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import first from '../../Images/ActivityImages/1A4F808C-E760-44EB-BC79-12A1D511A786_1_105_c.jpeg'
-import second from '../../Images/ActivityImages/30B2E031-7B86-4538-8EA3-7DFD91C56FF9_1_105_c.jpeg'
-import third from '../../Images/ActivityImages/34FE7937-CB29-4AD2-9A53-A20929F972E7_1_105_c.jpeg'
-import fifth from '../../Images/ActivityImages/74A28346-1FF6-40DB-A6F9-110950C08D4F_1_105_c.jpeg'
-import sixth from '../../Images/ActivityImages/7645496A-64EF-4ACA-A9C4-0A827BAD8F79_1_105_c.jpeg'
-import seventh from '../../Images/ActivityImages/778BDF44-921F-4D23-90AE-EC57A32EAC81_1_105_c.jpeg'
-import eighth from '../../Images/ActivityImages/93E06524-7E7B-4BD2-91B6-F1D49BB39167.jpeg'
-import ninth from '../../Images/ActivityImages/9BF9195D-1EA5-4179-B18D-E7762B01766E_1_105_c.jpeg'
-import tenth from '../../Images/ActivityImages/9FC1B9CB-C075-4884-8106-24D1BA7762DD_1_105_c.jpeg'
-import twelfth from '../../Images/ActivityImages/BE5063A8-66B3-451C-9C36-ECFEF860B2F6_1_105_c.jpeg'
-import thirteenth from '../../Images/ActivityImages/EDCC82AF-7336-442C-A1B0-815A58980778_1_105_c.jpeg'
-import fourteenth from '../../Images/ActivityImages/FD240375-325D-45CA-A902-D389A69238D9_1_105_c.jpeg'
-import fifteenth from '../../Images/ActivityImages/FEF979ED-5331-4DCF-B606-6467A5DA34A4.jpeg'
+import React, { useRef } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import first from "../../Images/ActivityImages/1A4F808C-E760-44EB-BC79-12A1D511A786_1_105_c.jpeg";
+import second from "../../Images/ActivityImages/30B2E031-7B86-4538-8EA3-7DFD91C56FF9_1_105_c.jpeg";
+import third from "../../Images/ActivityImages/34FE7937-CB29-4AD2-9A53-A20929F972E7_1_105_c.jpeg";
+import fifth from "../../Images/ActivityImages/74A28346-1FF6-40DB-A6F9-110950C08D4F_1_105_c.jpeg";
+import sixth from "../../Images/ActivityImages/7645496A-64EF-4ACA-A9C4-0A827BAD8F79_1_105_c.jpeg";
+import seventh from "../../Images/ActivityImages/778BDF44-921F-4D23-90AE-EC57A32EAC81_1_105_c.jpeg";
+import eighth from "../../Images/ActivityImages/93E06524-7E7B-4BD2-91B6-F1D49BB39167.jpeg";
+import ninth from "../../Images/ActivityImages/9BF9195D-1EA5-4179-B18D-E7762B01766E_1_105_c.jpeg";
+import tenth from "../../Images/ActivityImages/9FC1B9CB-C075-4884-8106-24D1BA7762DD_1_105_c.jpeg";
+import twelfth from "../../Images/ActivityImages/BE5063A8-66B3-451C-9C36-ECFEF860B2F6_1_105_c.jpeg";
+import thirteenth from "../../Images/ActivityImages/EDCC82AF-7336-442C-A1B0-815A58980778_1_105_c.jpeg";
+import fourteenth from "../../Images/ActivityImages/FD240375-325D-45CA-A902-D389A69238D9_1_105_c.jpeg";
+import fifteenth from "../../Images/ActivityImages/FEF979ED-5331-4DCF-B606-6467A5DA34A4.jpeg";
 
 const ActivityCarousel = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        accessibility: true,
-        autoplay: true,
-        autoplaySpeed: 6000,
-        cssEase: "ease",
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                className: "center",
-                centerMode: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-              }
-            }
-          ]
-      };
-    
-      const images = [
-        first, // Adjusted placeholder image size
-        second,
-        third,
-        fifth,
-        sixth,
-        seventh,
-        eighth,
-        ninth,
-        tenth,
-        twelfth,
-        thirteenth,
-        fourteenth,
-        fifteenth
-      ];
-    
-      return (
-        <div className="container mx-auto mb-8 mt-8">
-          <Slider {...settings}>
-            {images.map((image, index) => (
-              <div key={index} className="px-2">
-                <div className="relative w-[244px] h-[360px] bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300">
-                  <img
-                    src={image}
-                    alt={`Slide ${index + 1}`}
-                    className="inset-0 w-full h-full object-cover rounded-lg shadow-lg"
-                  />
-                </div>
-              </div>
-            ))}
-          </Slider>
-        </div>
-      );
-    };
+  const sliderRef = useRef(null);
 
-export default ActivityCarousel
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    accessibility: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    cssEase: "ease",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          className: "center",
+          centerMode: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const images = [
+    { src: first, alt: "The inside of BC Place during a WhiteCaps soccer game" },
+    { src: second, alt: "The inside of Rogers Arena during a Canucks hockey game" },
+    { src: third, alt: "A small mountaineering shack peeking over the top of Panorama Ridge Mountain" },
+    { src: fifth, alt: "A black and white kitten (Poppy!) sitting smugly on a laptop" },
+    { src: sixth, alt: "A blonde girl (RuthAnne!) smirking at the camera while on a boardwalk in a field surrounded by mountains" },
+    { src: seventh, alt: "An outdoor Canadians baseball game at Nat Bailey Stadium" },
+    { src: eighth, alt: "A man and woman, (Me and Ruthie!), smiling atop a mountain overlooking a large lake" },
+    { src: ninth, alt: "A man in active wear (Me!) running on the Vancouver sea wall" },
+    { src: tenth, alt: "A woman in the distance (Ruthie!) walking on the ridgeline of a snowy mountain " },
+    { src: twelfth, alt: "A gondola passing through the trees of Grouse Mountain" },
+    { src: thirteenth, alt: "Two tents on a wooden platform in light forest cover, mountains can be seen in the background" },
+    { src: fourteenth, alt: "The SkyTrain pulling into Olympic Village station on a sunny day" },
+    { src: fifteenth, alt: "A man and woman (Me and Ruthie!) smiling infront of a large waterfall" },
+  ];
+
+  const handleKeyDown = (event) => {
+    if (event.key === "ArrowLeft") {
+      sliderRef.current.slickPrev();
+    } else if (event.key === "ArrowRight") {
+      sliderRef.current.slickNext();
+    }
+  };
+
+  return (
+    <div
+      className="container mx-auto mb-8 mt-8 relative"
+      tabIndex={0}
+      onKeyDown={handleKeyDown}
+    >
+      <button
+        aria-label="Previous slide"
+        className="absolute left-[-40px] top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full z-10 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        onClick={() => sliderRef.current.slickPrev()}
+      >
+        &lt;
+      </button>
+      <Slider ref={sliderRef} {...settings}>
+        {images.map((image, index) => (
+          <div key={index} className="px-2">
+            <div className="relative w-[244px] h-[360px] bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition-shadow duration-300">
+              <img
+                src={image.src}
+                alt={image.alt}
+                className="inset-0 w-full h-full object-cover rounded-lg shadow-lg"
+              />
+            </div>
+          </div>
+        ))}
+      </Slider>
+      <button
+        aria-label="Next slide"
+        className="absolute right-[-40px] top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full z-10 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        onClick={() => sliderRef.current.slickNext()}
+      >
+        &gt;
+      </button>
+    </div>
+  );
+};
+
+export default ActivityCarousel;
--- src/components/AboutComponents/TiltCardCarousel.jsx
+++ src/components/AboutComponents/TiltCardCarousel.jsx
@@ -1,5 +1,5 @@
 // src/components/TiltCardCarousel.jsx
-import React from 'react';
+import React, { useRef } from 'react';
 import Slider from 'react-slick';
 import TiltCard from './TiltCard';
 import "slick-carousel/slick/slick.css";
@@ -17,19 +17,7 @@ import okaun from '../../Images/MTGImages/Okaun, Eye of Chaos (sld1) (1LpubdYNqC
 import rinseri from '../../Images/MTGImages/Rin and Seri, Inseparable (1JdYFCG7fFo7NBzYXbvPN0PJLNT-3d0J-).png'
 
 const TiltCardCarousel = () => {
-  const cards = [
-    { id: 1, title: 'Atog', description: 'This is the first card.', imageUrl: atog, linkUrl: 'https://archidekt.com/decks/6466202/atogs_feast_of_success' },
-    { id: 2, title: 'Breya', description: 'This is the second card.', imageUrl: breya, linkUrl: 'https://archidekt.com/decks/5264238/if_she_breathe_she_a_thopt' },
-    { id: 3, title: 'Chulane', description: 'This is the third card.', imageUrl: chulane, linkUrl: 'https://archidekt.com/decks/5264140/bant_solitaire' },
-    { id: 4, title: 'FireSong', description: 'This is the fourth card.', imageUrl: firesong, linkUrl: 'https://archidekt.com/decks/5264217/masochistic_minotaurs' },
-    { id: 5, title: 'Geralf', description: 'This is the fifth card.', imageUrl: geralf, linkUrl: 'https://archidekt.com/decks/7822160/geralf_needs_his_cereal' },
-    { id: 6, title: 'Gishath', description: 'This is the sixth card.', imageUrl: gishath, linkUrl: 'https://archidekt.com/decks/7118593/phillips_dinosour' },
-    { id: 7, title: 'Grenzo', description: 'This is the seventh card.', imageUrl: grenzo, linkUrl: 'https://archidekt.com/decks/6012940/grenzo_christmas_warden' },
-    { id: 8, title: 'Horde', description: 'This is the eight card.', imageUrl: horde, linkUrl: 'https://archidekt.com/decks/5264226/gaslight_gatekeep_girlboss' },
-    { id: 9, title: 'Meren', description: 'This is the ninth card.', imageUrl: meren, linkUrl: 'https://archidekt.com/decks/5264206/die_another_day' },
-    { id: 10, title: 'Okaun and Zndrsplt', description: 'This is the tenth card.', imageUrl: okaun, linkUrl: 'https://archidekt.com/decks/5724722/heads_i_win_tails_you_lose' },
-    { id: 11, title: 'Rin and Seri', description: 'This is the eleventh card.', imageUrl: rinseri, linkUrl: 'https://archidekt.com/decks/5288599/its_raining_these_guys' }
-  ];
+  const sliderRef = useRef(null);
   
   const settings = {
     dots: true,
@@ -42,41 +30,88 @@ const TiltCardCarousel = () => {
     autoplaySpeed: 6000,
     cssEase: "ease",
     responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            className: "center",
-            centerMode: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          }
+      {
+        breakpoint: 1024,
+        settings: { 
+          slidesToShow: 3, 
+          slidesToScroll: 1 
         }
-      ]
+      },
+      {
+        breakpoint: 600,
+        settings: { 
+          slidesToShow: 2, 
+          slidesToScroll: 1 
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: { 
+          className: "center",
+          centerMode: true,
+          slidesToShow: 1, 
+          slidesToScroll: 1
+         }
+      }
+    ]
+  };
+
+  const cards = [
+    { id: 1, title: 'Atog', description: 'Magic the Gathering Card: Atogatog', imageUrl: atog, linkUrl: 'https://archidekt.com/decks/6466202/atogs_feast_of_success' },
+    { id: 2, title: 'Breya', description: 'Magic the Gathering Card: Breya, the etherium shaper', imageUrl: breya, linkUrl: 'https://archidekt.com/decks/5264238/if_she_breathe_she_a_thopt' },
+    { id: 3, title: 'Chulane', description: 'Magic the Gathering Card: Chulane, Teller of tales', imageUrl: chulane, linkUrl: 'https://archidekt.com/decks/5264140/bant_solitaire' },
+    { id: 4, title: 'FireSong', description: 'Magic the Gathering Card: Firesong and Sunspeaker', imageUrl: firesong, linkUrl: 'https://archidekt.com/decks/5264217/masochistic_minotaurs' },
+    { id: 5, title: 'Geralf', description: 'Magic the Gathering Card: Geralf the Fleshwright', imageUrl: geralf, linkUrl: 'https://archidekt.com/decks/7822160/geralf_needs_his_cereal' },
+    { id: 6, title: 'Gishath', description: 'Magic the Gathering Card: Gishath, Suns avatar', imageUrl: gishath, linkUrl: 'https://archidekt.com/decks/7118593/phillips_dinosour' },
+    { id: 7, title: 'Grenzo', description: 'Magic the Gathering Card: Grenzo, dungeon Warden', imageUrl: grenzo, linkUrl: 'https://archidekt.com/decks/6012940/grenzo_christmas_warden' },
+    { id: 8, title: 'Horde', description: 'Magic the Gathering Card: Horde of Notions', imageUrl: horde, linkUrl: 'https://archidekt.com/decks/5264226/gaslight_gatekeep_girlboss' },
+    { id: 9, title: 'Meren', description: 'Magic the Gathering Card: Meren of Clan Nel Toth', imageUrl: meren, linkUrl: 'https://archidekt.com/decks/5264206/die_another_day' },
+    { id: 10, title: 'Okaun and Zndrsplt', description: 'Magic the Gathering Card: Okaun Eye of Chaos', imageUrl: okaun, linkUrl: 'https://archidekt.com/decks/5724722/heads_i_win_tails_you_lose' },
+    { id: 11, title: 'Rin and Seri', description: 'Magic the Gathering Card: Rin and Seri Inseperable', imageUrl: rinseri, linkUrl: 'https://archidekt.com/decks/5288599/its_raining_these_guys' }
+  ];
+
+  const handleKeyDown = (event) => {
+    if (event.key === "ArrowLeft") {
+      sliderRef.current.slickPrev();
+    } else if (event.key === "ArrowRight") {
+      sliderRef.current.slickNext();
+    }
   };
 
   return (
-    <div className="container mx-auto mb-8 mt-8">
-      <Slider {...settings}>
-        {cards.map(card => (
-          <TiltCard key={card.id} title={card.title} description={card.description} imageUrl={card.imageUrl} linkUrl={card.linkUrl} />
+    <div
+      className="container mx-auto mb-8 mt-8 relative"
+      tabIndex={0}
+      onKeyDown={handleKeyDown}
+    >
+      <button
+        aria-label="Previous slide"
+        className="absolute left-[-40px] top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full z-10 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        onClick={() => sliderRef.current.slickPrev()}
+      >
+        &lt;
+      </button>
+      <Slider ref={sliderRef} {...settings}>
+        {cards.map((card) => (
+          <div key={card.id} tabIndex="0" aria-label={`Card ${card.title}`}>
+            <TiltCard
+              title={card.title}
+              description={card.description}
+              imageUrl={card.imageUrl}
+              linkUrl={card.linkUrl}
+            />
+          </div>
         ))}
       </Slider>
+      <button
+        aria-label="Next slide"
+        className="absolute right-[-40px] top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full z-10 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        onClick={() => sliderRef.current.slickNext()}
+      >
+        &gt;
+      </button>
     </div>
   );
-}
+};
 
 export default TiltCardCarousel;
